e778e0146715dbac7362c9d409785c6577419103
Konstantinos Artopoulos <artopouloskon@gmail.com>
2025-11-22 16:52:38 +0200

    feat: module autodiscovery

--- python/django_wsrs/management/commands/runwebsockets.py
+++ python/django_wsrs/management/commands/runwebsockets.py
@@ -1,8 +1,10 @@
 import django_wsrs
 
+from django.utils.module_loading import autodiscover_modules
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        autodiscover_modules("websockets", "sockets", "sse", "views")
         django_wsrs.run_server()
