0e90ef8b4aa96ae54fda7c1fda8f5e213da84077
Konstantinos Artopoulos <artopouloskon@gmail.com>
2025-12-30 22:56:05 +0200

    ops: migrate from shell.nix to flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "django-wsrs development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "django_wsrs";
+
+            buildInputs = with pkgs; [
+              rustc
+              cargo
+              python313
+              uv
+              gcc
+              gnumake
+              deno
+            ];
+
+            shellHook = ''
+              export PYO3_PYTHON=${pkgs.python313}/bin/python
+            '';
+          };
+        });
+    };
+}
